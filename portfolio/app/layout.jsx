773640d02f007b8d34357d8d3f7710c878702e81
Chanchala Jeewanthi Jayalath <jeewanthichanchala321@gmail.com>
2025-08-01 15:25:10 +0530

    update

--- portfolio/app/layout.jsx
+++ portfolio/app/layout.jsx
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const jetbrainsmono =JetBrains_Mono  ({ 
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"], 
+  variable: "--font-jetbrains-mono",
 });
 
 export const metadata = {
@@ -19,11 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={jetbrainsmono.variable}>{children}</body>
     </html>
   );
 }
