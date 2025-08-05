009b98431f6f636301235d93935cc60845fca17a
Chanchala Jeewanthi Jayalath <jeewanthichanchala321@gmail.com>
2025-08-05 12:29:08 +0530

    update

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react';  
+import Link from "next/link";
+import {Button} from "./ui/button";
+
 
 const Header = () => {
   return(
-    <header>header</header>
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">header</div>
+    </header>
   );
   
 };
