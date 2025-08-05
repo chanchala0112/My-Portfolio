b9dfd43dddf6deb7ccd55abdc06db16576ff01bf
Chanchala Jeewanthi Jayalath <jeewanthichanchala321@gmail.com>
2025-08-05 13:51:44 +0530

    update

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -4,7 +4,7 @@ import {Button} from "./ui/button";
 
 const Header = () => {
   return(
-    <header className="py-8 xl:py-12 text-white">
+    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
       <div className="container mx-auto">header</div>
     </header>
   );
